a05b739b0e829262f231d047ddf25ca5685ffb36
Odilon <your@email.com>
2023-10-08 07:46:17 +0100

    sesame

--- src/routes.js
+++ src/routes.js
@@ -35,8 +35,6 @@ const Range = React.lazy(() => import('./views/forms/range/Range'))
 const Select = React.lazy(() => import('./views/forms/select/Select'))
 const Validation = React.lazy(() => import('./views/forms/validation/Validation'))
 
-const Charts = React.lazy(() => import('./views/charts/Charts'))
-
 // Icons
 const CoreUIIcons = React.lazy(() => import('./views/icons/coreui-icons/CoreUIIcons'))
 const Flags = React.lazy(() => import('./views/icons/flags/Flags'))
@@ -76,7 +74,6 @@ const routes = [
   { path: '/usermanagement/UserManagement', name: 'UserManagement', element: UserManagement },
   { path: '/profile/Profile', name: 'Profile', element: Profile },
 
-  { path: '/charts', name: 'Charts', element: Charts },
   { path: '/forms', name: 'Forms', element: FormControl, exact: true },
   { path: '/forms/form-control', name: 'Form Control', element: FormControl },
   { path: '/forms/select', name: 'Select', element: Select },
